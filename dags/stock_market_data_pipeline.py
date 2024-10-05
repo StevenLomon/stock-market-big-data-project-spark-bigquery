07a34d54e348f3d965af5f061a1f619b673501c9
StevenLomon <steven.lennartsson@gmail.com>
2024-10-05 09:07:54 +0200

    First commit with ci.yml in place!

--- dags/stock_market_data_pipeline.py
+++ dags/stock_market_data_pipeline.py
@@ -16,3 +16,12 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+
+dag = DAG(
+    'stock_market_data_pipeline',
+    default_args=default_args,
+    description='An ETL pipeline for stock market data using Alpha Vantage, PySpark and BigQuery',
+    schedule_interval=(timedelta(days=1)),
+    start_date=datetime(2024, 10, 6),
+    catchup=False,
+)
